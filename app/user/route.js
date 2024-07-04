94bbe04267b2c24314b643caefeecfa76b2cedd2
Arul3011 <yarul8406@gmail.com>
2024-07-04 21:41:22 +0530

    first commit

---
+++ app/user/route.js
@@ -0,0 +1,21 @@
+import data from "../data/data";
+// const resdata = data[0];
+export function GET() {
+  return new Response(JSON.stringify({ data }));
+}
+
+export async function POST(response) {
+  const resdata = await response.json();
+  let valid = false;
+  if (resdata) {
+    data.map((val) => {
+      if (
+        (val.name === resdata.name || val.email === resdata.email) &&
+        val.password === resdata.password
+      ) {
+        valid = true;
+      }
+    });
+  }
+  return new Response(JSON.stringify({ response: valid }));
+}
